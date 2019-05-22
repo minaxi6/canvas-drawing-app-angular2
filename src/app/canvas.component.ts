7b690749c475789a8c515c9bb325c7cbe4f80017
Himani <himani.varshney@charpixel.com>
2019-05-22 12:50:22 +0530

    circle rectangle pencil draw

--- src/app/canvas.component.ts
+++ src/app/canvas.component.ts
@@ -17,16 +17,22 @@ export class CanvasComponent implements AfterViewInit {
   @Input() public width = 700;
   @Input() public height = 400;
   startX: number = null;
-  startY: number = null
+  startY: number = null;
+  cursor: boolean = false;
   mouseX: number = null;
   mouseY: number = null;
   drag = false;
   radius = 50;
+  color = "#000";
+  x: number = null;
+  y: number = null;
+  lastEvent = null;
 
   private cx: CanvasRenderingContext2D;
-  constructor(private el: ElementRef, private renderer: Renderer) {
 
-  }
+  drawerTypes = ["pencil", "erasor", "square", "circle"];
+  drawerType = this.drawerTypes[0];
+  constructor(private el: ElementRef, private renderer: Renderer) { }
   public ngAfterViewInit() {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
     this.cx = canvasEl.getContext('2d');
@@ -38,45 +44,46 @@ export class CanvasComponent implements AfterViewInit {
     this.cx.strokeStyle = '#000';
 
 
-    this.captureEvents(canvasEl);
+    // this.captureEvents(canvasEl);
   }
 
+  mouseDown(event) {
+    if (this.lastEvent == null) {
+      this.lastEvent = event;
+      return;
+    }
 
-  private captureEvents(canvasEl: HTMLCanvasElement) {
-    // this will capture all mousedown events from the canvas element
-    fromEvent(canvasEl, 'mousedown')
-      .pipe(
-      switchMap((e) => {
-        // after a mouse down, we'll record all mouse moves
-        return fromEvent(canvasEl, 'mousemove')
-          .pipe(
-          // we'll stop (and unsubscribe) once the user releases the mouse
-          // this will trigger a 'mouseup' event 
-          takeUntil(fromEvent(canvasEl, 'mouseup')),
-          // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
-          takeUntil(fromEvent(canvasEl, 'mouseleave')),
-          // pairwise lets us get the previous value to draw a line from
-          // the previous point to the current point    
-          pairwise()
-          )
-      })
-      )
-      .subscribe((res: [MouseEvent, MouseEvent]) => {
-        const rect = canvasEl.getBoundingClientRect();
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.drag = true;
+    console.log(this.drag);
+    this.lastEvent = event;
 
-        // previous and current position with the offset
-        const prevPos = {
-          x: res[0].clientX - rect.left,
-          y: res[0].clientY - rect.top
-        };
-
-        const currentPos = {
-          x: res[1].clientX - rect.left,
-          y: res[1].clientY - rect.top
-        };
-        this.drawOnCanvas(prevPos, currentPos);
-      });
   }
+
+  mouseUp(event) {
+    this.drag = false;
+
+  }
+
+  mouseMove(event) {
+
+    if (this.drawerType == "ractangle") {
+      this.rect(event);
+    }
+    else if (this.drawerType == "circle") {
+      this.drawCircle(this.x, this.y);
+    }
+
+    else if (this.drawerType == "erasor") {
+      this.drawLine(event, true);
+
+    } else {
+      this.drawLine(event, false);
+    }
+
+  }
+
 
   private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
     if (!this.cx) { return; }
@@ -84,16 +91,43 @@ export class CanvasComponent implements AfterViewInit {
     this.cx.beginPath();
 
     if (prevPos) {
-      //this.cx.moveTo(prevPos.x, prevPos.y);
-      //this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.moveTo(prevPos.x, prevPos.y);
+      this.cx.lineTo(currentPos.x, currentPos.y);
       this.cx.stroke();
     }
   }
 
-  eraseLine() {
-    console.log("hi------>>>")
-    this.cx.lineWidth = 5;
-    this.cx.strokeStyle = '#ffffff';
+  drawLine(event, isErasor) {
+
+    if (!this.drag) {
+      return;
+    }
+
+
+    console.log("draw line")
+
+    const canvas = event.target;
+    const rect = canvas.getBoundingClientRect();
+
+    const prevPos = {
+
+      x: this.lastEvent.clientX - rect.left,
+      y: this.lastEvent.clientY - rect.top
+    };
+    const currentPos = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };
+
+    this.cx.strokeStyle = isErasor ? "#ffffff" : this.color;
+
+    this.cx.beginPath();
+
+    this.cx.moveTo(prevPos.x, prevPos.y);
+    this.cx.lineTo(currentPos.x, currentPos.y);
+    this.cx.stroke();
+    this.lastEvent = event;
+
   }
   lineWidth(w) {
     this.cx.lineWidth = w;
@@ -101,53 +135,7 @@ export class CanvasComponent implements AfterViewInit {
 
   colorPencil(color) {
     this.cx.strokeStyle = color;
-  }
-
-
-  draw(color) {
-    if (this.cx) {
-      let canvas = this.canvas;
-      if (this.cx) {
-        var ctx = this.cx;
-        ctx.beginPath();
-        ctx.arc(this.width * .6, this.height * .6, this.width * .2, 0, Math.PI * 2, true);
-        ctx.strokeStyle = color;
-        ctx.stroke();
-        ctx.closePath();
-      }
-    }
-  }
-
-  mdEvent(e) {
-    //persist starting position
-    e.preventDefault();
-    e.stopPropagation();
-    var rect = this.canvas.nativeElement.getBoundingClientRect();
-    this.startX = e.clientX - rect.left;
-    this.startY = e.clientY - rect.top;
-    this.drag = true;
-  }
-
-  mmEvent(e) {
-
-    /// this.rect(e);
-    if (!this.drag) {
-      return;
-    }
-    e.preventDefault();
-    e.stopPropagation();
-    var rect = this.canvas.nativeElement.getBoundingClientRect();
-    this.mouseX = e.clientX - rect.left;
-    this.mouseY = e.clientY - rect.top;
-    this.drawCircle(this.mouseX, this.mouseY);
-
-  }
-
-  muEvent(e) {
-    if (!this.drag) { return; }
-    e.preventDefault();
-    e.stopPropagation();
-    this.drag = false;
+    this.color = color;
   }
 
   clearArea() {
@@ -155,22 +143,40 @@ export class CanvasComponent implements AfterViewInit {
     this.cx.clearRect(0, 0, this.width, this.height);
   }
 
-  rect(e) {
+  rect(event) {
+    if (!this.drag) {
+      return;
+    }
+
+    const canvas = event.target;
+
     if (this.drag) {
-      let x = this.startX - this.canvas.nativeElement.getBoundingClientRect().left;
-      let y = this.startY - this.canvas.nativeElement.getBoundingClientRect().top;
-      let w = e.clientX - this.canvas.nativeElement.getBoundingClientRect().left - x;
-      let h = e.clientY - this.canvas.nativeElement.getBoundingClientRect().top - y;
+
+
+      const rect = canvas.getBoundingClientRect();
+
+      let canvasLeft = rect.left, canvasTop = rect.top;
+      let x = this.startX - canvasLeft;
+      let y = this.startY - canvasTop;
+      let w = event.clientX - canvasLeft - x;
+      let h = event.clientY - canvasTop - y;
+      this.cx.beginPath();
+      this.cx.strokeStyle = this.color;
       this.cx.fillStyle = "#fff";
       this.cx.fillRect(x, y, w, h);
       this.cx.strokeRect(x, y, w, h);
+
     }
+
+    const rect = canvas.getBoundingClientRect();
+
+
 
   }
 
   drawCircle(x, y) {
 
-    this.cx.clearRect(0, 0, this.width, this.height);
+    // this.cx.clearRect(0, 0, this.width, this.height);
     this.cx.save();
     this.cx.beginPath();
     this.cx.moveTo(this.startX, this.startY + (y - this.startY) / 2);
@@ -181,18 +187,28 @@ export class CanvasComponent implements AfterViewInit {
     this.cx.fillStyle = "white";
     this.cx.fill();
     this.cx.stroke();
-    //this.cx.strokeStyle = this.getRandomColor();
   }
 
+  drawPencil() {
+    this.drawerType = "pencil"
+  }
+  rectangle() {
 
-  // getRandomColor() {
-  //   var letters = '0123456789ABCDEF';
-  //   var color = '#';
-  //   for (var i = 0; i < 6; i++) {
-  //     color += letters[Math.floor(Math.random() * 16)];
-  //   }
-  //   return color;
-  // }
+    this.drawerType = "ractangle"
+
+  }
+
+  eraseLine() {
+
+    this.drawerType = "erasor";
+    this.cx.lineWidth = 5;
+    this.cx.strokeStyle = '#ffffff';
+
+
+  }
+  circle() {
+    this.drawerType = "circle";
+  }
 
 
 }
